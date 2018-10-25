10ee26567cdd21df6e41e6fe64e6a7e0e1a35020
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-25 00:10:23 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,14 @@ class StudentsController < ApplicationController
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    set_student
+    !!@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
 end
